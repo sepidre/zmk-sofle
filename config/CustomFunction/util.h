734a19bfc0d06243dd9efec7180dfe601b68428e
Stephan <github@athome.zone>
2023-06-14 15:56:24 +0200

    clean code

---
+++ config/CustomFunction/util.h
@@ -0,0 +1,18 @@
+// Copyright 2022 jack (@waffle87)
+// SPDX-License-Identifier: MIT
+
+#define COMBO(name, keypress, keypos) \
+combo_##name {                        \
+   timeout-ms = <50>;                 \
+   bindings = <keypress>;             \
+   key-positions = <keypos>;          \
+};
+
+#define TAP_DANCE(name, keypress1, keypress2, keypress3) \
+td_##name: name {                             \
+  compatible = "zmk,behavior-tap-dance";      \
+  label = ###name;                            \
+  #binding-cells = <0>;                       \
+  tapping-term-ms = <200>;                    \
+  bindings = <keypress1>, <keypress2>, <keypress3>;        \
+};
